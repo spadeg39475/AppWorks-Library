bcb136caba38061855830e39ce872fb3c4eadd97
Josh Yang <spadeg39475@gmail.com>
2019-11-29 11:05:03 +0800

    add List.test

--- src/__test__/List.test.js
+++ src/__test__/List.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import List from '../components/List';
-import AppContextProvider from '../context/AppContext';
+import { AppContext } from '../context/AppContext';
 
 beforeEach(cleanup); // clean the DOM!
 
@@ -11,18 +11,29 @@ jest.mock('../firebase', () => ({
   },
 }));
 
+function renderList(query, searchResult) {
+  return render(
+    <AppContext.Provider value= {{query,searchResult}}>
+      <List />
+    </AppContext.Provider>
+  );
+}
+
 
 describe('<List />', () => {
     it('render the list', () => {
-      
-      const { getByText } = render(
-        <AppContextProvider 
-        value={searchResult=[{title:'title', author: 'author', status: 'borrored'}],
-        query={tag: 'title', value:'白夜行'}}
-        >
-            <List />
-        </AppContextProvider>);
-    
-        expect( getByText('借閱狀態')).toBeTruthy();
+      const query = {
+        tag: 'title', 
+        value:'白夜行'
+      };
+      const searchResult = [{ 
+        'title': '白夜行',
+        'author': '東野圭吾',
+         'id': '12345',
+         'status': 'return'
+       }]
+
+      const { getByText } = renderList(query,searchResult)     
+      expect( getByText('可借閱')).toBeTruthy();
     });
-  });
+});
